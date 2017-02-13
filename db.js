d6e38ca8dfdc36d068c8fd1c96f0d21652ffc2d2
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-02-12 17:04:11 -0800

    version before checking

---
+++ db.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+var file = "music.db";
+var exists = fs.existsSync(file);
+var models = require('./models');
+
+models.sequelize.sync({force: true}).then(function() {
+    var songsObj;
+
+    if(!exists) {
+        fs.readFile('./songs.json', function(err, data) {
+            var music_data = JSON.parse(data);
+            var songs = music_data['songs'];
+            songsObj = songs;
+
+            songs.forEach(function(song) {
+                // console.log(song);
+                models.Song.create({
+                    id: song.id.toString(),
+                    title: song.title,
+                    album: song.album,
+                    artist: song.artist,
+                    duration: song.duration
+                });
+            });
+        });
+
+        fs.readFile('./playlists.json', function(err, data) {
+            var songId, i;
+            var music_data = JSON.parse(data);
+            var playlists = music_data['playlists'];
+
+            playlists.forEach(function(playlist) {
+                // console.log(playlist);
+                models.Playlist.create({
+                    id: playlist.id.toString(),
+                    name: playlist.name
+                }).then(function(PlaylistInstance) {
+                    for (i = 0; i < playlist.songs.length; i++) {
+                        songId = playlist.songs[i];
+                        models.Song.findById(songId).then(function(song) {
+                            PlaylistInstance.addSong(song);
+                        })
+                    }
+                });
+            });
+        });
+    } else {
+        console.log("Database creation failed: database exists already.");
+    }
+});
